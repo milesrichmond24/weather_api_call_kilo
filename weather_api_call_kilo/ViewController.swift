5ff5df0fe596ee7a21c78086fc68a1d28587c247
miles.richmond24 <miles.richmond24@d155.org>
2024-01-11 18:04:01 -0600

    Initial Commit

---
+++ weather_api_call_kilo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather_api_call_kilo
+//
+//  Created by Miles Richmond on 1/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ weather_api_call_kiloUITests/weather_api_call_kiloUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  weather_api_call_kiloUITestsLaunchTests.swift
+//  weather_api_call_kiloUITests
+//
+//  Created by Miles Richmond on 1/11/24.
+//
+
+import XCTest
+
+class weather_api_call_kiloUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
